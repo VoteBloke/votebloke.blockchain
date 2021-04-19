d0800999ec80adc9a327c8db1e8ca33b446630a9
Konrad Pagacz <konrad.pagacz@gmail.com>
2021-04-19 23:01:04 +0200

    lint-checks

--- blockchain/test/java/blockchain/ChainTest.java
+++ blockchain/test/java/blockchain/ChainTest.java
@@ -6,65 +6,64 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChainTest {
-    Chain blockchain;
+  Chain blockchain;
 
-    @BeforeEach
-    void setUp() {
-        Block genesisBlock = new Block("0", "v1", 0);
-        genesisBlock.mineHash();
-        blockchain = new Chain(genesisBlock);
-    }
+  @BeforeEach
+  void setUp() {
+    Block genesisBlock = new Block("0", "v1", 0);
+    genesisBlock.mineHash();
+    blockchain = new Chain(genesisBlock);
+  }
 
-    @Test
-    void isChainValid_OnlyGenesis() {
-        // Chain with only the genesisBlock is valid
-        assertTrue(blockchain.isChainValid());
-    }
+  @Test
+  void isChainValid_OnlyGenesis() {
+    // Chain with only the genesisBlock is valid
+    assertTrue(blockchain.isChainValid());
+  }
 
-    @Test
-    void isChainValid_WrongPreviousHash() {
-        // Chain with a block with a wrong previousHash is not valid
-        Block newBlock = new Block("something", "v1", 0);
-        newBlock.mineHash();
-        blockchain.addBlock(newBlock);
-        assertFalse(blockchain.isChainValid());
-    }
+  @Test
+  void isChainValid_WrongPreviousHash() {
+    // Chain with a block with a wrong previousHash is not valid
+    Block newBlock = new Block("something", "v1", 0);
+    newBlock.mineHash();
+    blockchain.addBlock(newBlock);
+    assertFalse(blockchain.isChainValid());
+  }
 
-    @Test
-    void addBlock_InvalidBlocksNotAdded() {
-        // Invalid blocks are not added to the Chain
-        int oldLength = blockchain.size();
-        Block invalidBlock = new Block(blockchain.getLatestBlockHash(), "v1", 0);
-        assertFalse(blockchain.addBlock(invalidBlock));
-        assertEquals(oldLength, blockchain.size());
-    }
+  @Test
+  void addBlock_InvalidBlocksNotAdded() {
+    // Invalid blocks are not added to the Chain
+    int oldLength = blockchain.size();
+    Block invalidBlock = new Block(blockchain.getLatestBlockHash(), "v1", 0);
+    assertFalse(blockchain.addBlock(invalidBlock));
+    assertEquals(oldLength, blockchain.size());
+  }
 
-    @Test
-    void addBlock_ReturnsTrueIfAdded() {
-        // addBlock returns true if the block is added and chain length is incremented
-        Block validBlock = new Block(blockchain.getLatestBlockHash(), "v1", 0);
-        validBlock.mineHash();
-        assertTrue(blockchain.addBlock(validBlock));
-    }
+  @Test
+  void addBlock_ReturnsTrueIfAdded() {
+    // addBlock returns true if the block is added and chain length is incremented
+    Block validBlock = new Block(blockchain.getLatestBlockHash(), "v1", 0);
+    validBlock.mineHash();
+    assertTrue(blockchain.addBlock(validBlock));
+  }
 
-    @Test
-    void addBlock_UpdatesLatestHash() {
-        // addBlock updates the latestHash field
-        Block newBlock = new Block(blockchain.getLatestBlockHash(), "v1", 0);
-        newBlock.mineHash();
-        blockchain.addBlock(newBlock);
-        assertEquals(newBlock.getHash(), blockchain.getLatestBlockHash());
-    }
+  @Test
+  void addBlock_UpdatesLatestHash() {
+    // addBlock updates the latestHash field
+    Block newBlock = new Block(blockchain.getLatestBlockHash(), "v1", 0);
+    newBlock.mineHash();
+    blockchain.addBlock(newBlock);
+    assertEquals(newBlock.getHash(), blockchain.getLatestBlockHash());
+  }
 
-    @Test
-    void addBlock_IncrementsChainLength() {
-        // addBlock increments the chain length
-        Block newBlock = new Block(blockchain.getLatestBlockHash(), "v1", 0);
-        newBlock.mineHash();
-        int oldSize = blockchain.size();
-        blockchain.addBlock(newBlock);
-        assertEquals(oldSize + 1, blockchain.size());
-        assertEquals(2, blockchain.size());
-    }
-
+  @Test
+  void addBlock_IncrementsChainLength() {
+    // addBlock increments the chain length
+    Block newBlock = new Block(blockchain.getLatestBlockHash(), "v1", 0);
+    newBlock.mineHash();
+    int oldSize = blockchain.size();
+    blockchain.addBlock(newBlock);
+    assertEquals(oldSize + 1, blockchain.size());
+    assertEquals(2, blockchain.size());
+  }
 }
