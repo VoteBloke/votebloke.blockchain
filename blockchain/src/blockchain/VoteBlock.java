3092f4bafbcbe26f42ad1c6a7614443a37e52764
Konrad Pagacz <konrad.pagacz@gmail.com>
2021-04-23 23:00:03 +0200

    Update VoteBlock

--- blockchain/src/blockchain/VoteBlock.java
+++ blockchain/src/blockchain/VoteBlock.java
@@ -1,5 +1,7 @@
 package blockchain;
 
+import java.util.List;
+
 public class VoteBlock extends Block {
   /**
    * Constructor for the Block class.
@@ -7,8 +9,13 @@ public class VoteBlock extends Block {
    * @param previousHash the hash of the previous Block
    * @param blockVersion the version of this Block
    * @param miningDifficulty the mining difficulty of this Block
+   * @param unconsumedTransactions the list of unconsumed TransactionOutputs
    */
-  public VoteBlock(String previousHash, String blockVersion, int miningDifficulty) {
-    super(previousHash, blockVersion, miningDifficulty);
+  public VoteBlock(
+      String previousHash,
+      String blockVersion,
+      int miningDifficulty,
+      List<TransactionOutput> unconsumedTransactions) {
+    super(previousHash, blockVersion, miningDifficulty, unconsumedTransactions);
   }
 }
