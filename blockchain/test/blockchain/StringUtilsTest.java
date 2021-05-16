abc1f67e358600868d6acab4a806ea8c194d64b5
Konrad Pagacz <konrad.pagacz@gmail.com>
2021-05-16 19:56:47 +0200

    Migrated to gradle

--- blockchain/test/blockchain/StringUtilsTest.java
+++
@@ -1,3 +0,0 @@
-package blockchain;
-
-class StringUtilsTest {}
---
+++ src/test/java/org/votebloke/blockchain/TransactionTest.java
@@ -0,0 +1,15 @@
+package org.votebloke.blockchain;
+
+import org.junit.jupiter.api.Test;
+
+class TransactionTest {
+
+  @Test
+  void processTransaction() {}
+
+  @Test
+  void generateSignature() {}
+
+  @Test
+  void verifySignature() {}
+}
