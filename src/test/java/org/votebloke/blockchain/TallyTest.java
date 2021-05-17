bbaed27fe36e6cd1dce783f1dceb6a2fb57c7b2b
Konrad Pagacz <konrad.pagacz@gmail.com>
2021-05-18 01:22:42 +0200

    Linting

--- src/test/java/org/votebloke/blockchain/TallyTest.java
+++ src/test/java/org/votebloke/blockchain/TallyTest.java
@@ -1,5 +1,7 @@
 package org.votebloke.blockchain;
 
+import static org.mockito.Mockito.when;
+
 import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
@@ -13,8 +15,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TallyTest {
