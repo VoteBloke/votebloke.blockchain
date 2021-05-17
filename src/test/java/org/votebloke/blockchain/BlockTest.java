57680bb0440162e62fc884b87479244bce15f945
Konrad Pagacz <konrad.pagacz@gmail.com>
2021-05-18 01:20:33 +0200

    Linting

--- src/test/java/org/votebloke/blockchain/BlockTest.java
+++ src/test/java/org/votebloke/blockchain/BlockTest.java
@@ -1,12 +1,10 @@
 package org.votebloke.blockchain;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 class BlockTest {
   Block block;
